1817e6e1c65d11438c94b7907e39885893fc4eb2
SwetyCore <swety2003@outlook.com>
2022-08-27 14:48:04 +0800

    添加项目文件。

---
+++ PU_Test/GlobalValues.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PU_Test
+{
+    internal static class GlobalValues
+    {
+        public static Frame frame;
+    }
+}
